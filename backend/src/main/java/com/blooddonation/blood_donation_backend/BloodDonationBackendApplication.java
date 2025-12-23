085eb4f863e9f8df2e413c91a0d5e9bd3c789315
Krishna teja 0904 <140784454+teja0604@users.noreply.github.com>
2025-12-23 23:42:13 +0530

    Issues Solving

--- backend/src/main/java/com/blooddonation/blood_donation_backend/BloodDonationBackendApplication.java
+++ backend/src/main/java/com/blooddonation/blood_donation_backend/BloodDonationBackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BloodDonationBackendApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(BloodDonationBackendApplication.class, args);
     }
